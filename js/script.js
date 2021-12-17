b315f491747106d22af04c57da3b79cade212843
ChewShinMin <94211048+ChewShinMin@users.noreply.github.com>
2021-12-17 21:43:06 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".menu");
-
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("change");
-});
